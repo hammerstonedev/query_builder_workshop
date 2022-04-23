e5c6d8c1faadcda80445cfdef0f0eff7b4c1e6c0
leenyburger <colleen@bitmappeddesigns.com>
2022-04-23 10:02:27 -0700

    Add Hammerstone routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,17 @@ Rails.application.routes.draw do
     # The root `/` path is routed to `Public::HomeController#index` by default.
   end
 
+  # Hammerstone routes 
+  namespace :hammerstone do
+    resource :refine_blueprint, only: [:show, :update, :create]
+    put "update_stable_id", to: "refine_blueprints#update_stable_id"
+    namespace :refine do
+      resources :stored_filters, only: [:create, :index, :show, :new, :update, :edit] do
+        get "editor", on: :collection
+      end
+    end
+  end
+
   namespace :webhooks do
     namespace :incoming do
       resources :bullet_train_webhooks
