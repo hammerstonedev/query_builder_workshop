c97987f329e58a9e217a78c62c536932701b5542
leenyburger <colleen@bitmappeddesigns.com>
2022-05-06 11:14:23 -0700

    Add seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,7 +1,37 @@
 puts "🌱 Generating development environment seeds."
+
+# Basic user with team and workspace
+puts
+puts "### Setting up users ###"
+puts
+user = User.find_or_create_by(email: "test@example.com") do |user|
+  user.first_name = "Example"
+  user.last_name = "User"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.time_zone = "Pacific Time (US & Canada)"
+end
+
+if user.current_team.nil?
+  user.create_default_team
+  user.save
+end
+
+puts
+puts "You can sign into the #{user.current_team.name} account with the following credentials."
+puts "Email: #{user.email}"
+puts "Password: password"
+puts
+
+puts
+puts "### Finished setting up users ###"
+puts
+
+
+
 require 'faker'
 # Create a team called "Query Builder Team" in the UI and then run this seeds file. 
-team = Team.find_or_create_by(name: "Query Builder Team")
+team = user.current_team
 20.times do
   contact = Contact.find_or_create_by(
     first_name: Faker::Name.first_name,
